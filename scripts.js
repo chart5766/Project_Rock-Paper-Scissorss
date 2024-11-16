05c1ad0f8fe8642510507c7a9b133efe94751b0a
chart5766 <68448700+chart5766@users.noreply.github.com>
2024-11-16 15:27:06 -0600

    Added function playRound and completed all scenarios

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
-
+let humanScore = 0;
+let computerScore = 0;
 
 
 function getComputerChoice() {
@@ -26,11 +27,75 @@ function getComputerChoice() {
 function getHumanChoice() {
 
     let result = prompt("Rock, Paper, Scissors")
+
+    result = result.toLowerCase();
+
+
     console.log(result)
 
 }
 
 
+function playRound(getHumanChoice, getComputerChoice) {
+
+    if (getHumanChoice === getComputerChoice) {
+
+        
+        console.log("Draw");
+
+    }
+
+
+    if (getHumanChoice === "rock"){
+
+        if (getComputerChoice === "paper") {
+
+            console.log("Paper beats rock");
+            computerScore += 1;
+        }
+
+        if (getComputerChoice === "scissors") {
+
+            console.log("Rock beats scissors");
+            humanScore += 1;
+        }
+    }
+
+
+    if (getHumanChoice === "paper"){
+
+        if (getComputerChoice === "rock") {
+
+            console.log("Paper beats rock");
+            humanScore += 1;
+        }
+
+        if (getComputerChoice === "scissors") {
+
+            console.log("Scissors beat paper");
+            computerScore += 1;
+        }
+    }
+
+    if (getHumanChoice === "scissors") {
+
+        if (getComputerChoice === "rock") {
+
+            console.log("Rock beats scissors");
+            computerScore += 1;
+        }
+
+        if (getComputerChoice === "paper") {
+
+            console.log("Scissors beat paper");
+            humanScore += 1;
+        }
+    }
+
+    console.log(humanScore);
+    console.log(computerScore)
+
+}
 
 
 
