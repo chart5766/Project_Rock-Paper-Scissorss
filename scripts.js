5a652ba6ce76a1f79586f6fc886ea6db298c3b9e
chart5766 <8448700+chart5766@users.noreply.github.com>
2024-11-16 01:59:13 -0600

    added if/else statements for getComputerChoice  function

---
+++ scripts.js
@@ -0,0 +1,33 @@
+
+
+
+function getComputerChoice() {
+
+    let result = Math.random() * 3;
+
+    if (result < 1) {
+
+        console.log("Rock");
+    }
+
+    if (result >= 1 && result < 2) {
+
+        console.log("Paper");
+    }
+
+    if (result >= 2) {
+
+        console.log("Scissors")
+    }
+
+
+
+}
+
+
+function getHumanChoice() {
+
+
+}
+
+getComputerChoice();
